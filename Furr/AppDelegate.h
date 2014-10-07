644953e3198bb1e642181a97feb45592564e063c
yummsalad <s0363027@student.rockvalleycollege.edu>
2014-10-06 22:55:57 -0500

    Furr
    website for app furr

---
+++ Furr/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Furr
+//
+//  Created by Cesar Silva on 10/6/14.
+//  Copyright (c) 2014 Cesar Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Furr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Furr
+//
+//  Created by Cesar Silva on 10/6/14.
+//  Copyright (c) 2014 Cesar Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
